12fc120d7f8005edbbb13d68e362706873bc97ad
vunk <silva.matheusr@gmail.com>
2024-07-11 22:09:14 -0300

    Parcial

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
+const msgContainer = document.querySelector('.msg-container');
+
+
+
+
+
+
